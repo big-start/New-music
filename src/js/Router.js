115c8f571addf2451dc67968d5533e79ab6d3390
hunter-s70 <serg.buhtoyarov@gmail.com>
2018-11-17 14:16:44 +0300

    add error pages, restrict Router lib for this

--- src/js/Router.js
+++ src/js/Router.js
@@ -8,117 +8,151 @@
 
 export default function (options) {
   const routes = options.routes;
+  const helper = createHelper({routes});
   
-  this.init = (() => setRoute(routes, {path: location.pathname}))();
+  this.init = (() => helper.setRoute(routes, {path: location.pathname}))();
   
   this.push = (route) => {
-    findRoute(routes, {path: route}).then((route) => {
+    helper.findRoute(routes, {path: route}).then((route) => {
       if (route.fullPath !== location.pathname) {
-        pushRoute(routes, route);
+        helper.pushRoute(routes, route);
       }
     });
   };
   
   window.addEventListener('popstate', function(e){
     if (e.state.route) {
-      setRoute(routes, {path: e.state.route});
+      helper.setRoute(routes, {path: e.state.route});
     }
   }, false);
 };
 
-function findRoute(routes, route) {
-  return new Promise((resolve) => {
-    for (let i = 0, max = routes.length; i < max; i+=1) {
-      if (routes[i].path === route.path) {
-        routes[i].fullPath = routes[i].path;
-        return resolve(routes[i]);
-      }
-      if (routes[i].children && routes[i].children.length) {
-        findRoute(routes[i].children, route).then((route) => {
-          // TODO level 3+ nested function
-          route.fullPath = routes[i].path + route.path;
-          return resolve(route);
-        });
-      }
-    }
-  });
-}
 
-function setRoute(routes, route) {
-  const pathArr = route.path.split('/').splice(1);
-  let contArr = [];
-  let tempArr = [];
-  let renderQueue = [];
-  pathArr.forEach((path) => {
-    const promise = findRoute(routes, {path: `/${path}`}).then((route) => {
-      return new Promise((resolve) => {
-        if (route.component) {
-          route.component().then((module) => {
-            contArr.push(module.default().context);
-            tempArr.push(module.default().template);
-            resolve();
-          })
-        }
-        if (route.redirect) {
-          pushRoute(routes, route);
-          resolve();
+/*
+* Private function context
+* */
+function createHelper(props) {
+  const routesArr = [];
+  return {
+    // Properties
+    routes: props.routes,
+    routesArr: (function genRoutesArr(routes) {
+      genRoutesArr.routesArr = genRoutesArr.routesArr || [];
+      routes.forEach((route) => {
+        if (routesArr.indexOf(route.path) === -1) {
+          genRoutesArr.routesArr.push(route.path);
+          if (route.children && route.children.length) {
+            genRoutesArr(route.children);
+          }
         }
       });
-    });
-    renderQueue.push(promise);
-  });
-  Promise.all(renderQueue).then(() => {
-    if (contArr.length && tempArr.length) {
-      renderAfterReload(contArr, tempArr);
+      return genRoutesArr.routesArr;
+    }(props.routes)),
+
+    // Methods
+    findRoute(routes, route) {
+      if (this.isRouteExist(route)) {
+        return new Promise((resolve) => {
+          for (let i = 0, max = routes.length; i < max; i+=1) {
+            if (routes[i].path === route.path) {
+              routes[i].fullPath = routes[i].path;
+              return resolve(routes[i]);
+            }
+            if (routes[i].children && routes[i].children.length) {
+              this.findRoute(routes[i].children, route).then((route) => {
+                // TODO level 3+ nested function
+                route.fullPath = routes[i].path + route.path;
+                return resolve(route);
+              });
+            }
+          }
+        });
+      } else {
+        return new Promise((resolve) => {
+          for (let i = 0, max = routes.length; i < max; i+=1) {
+            if (routes[i].path === '*') {
+              return resolve(routes[i]);
+            }
+          }
+        });
+      }
+    },
+    isRouteExist(route) {
+      return this.routesArr.indexOf(route.path) !== -1;
+    },
+    setRoute(routes, route) {
+      const pathArr = route.path.split('/').splice(1);
+      let contArr = [];
+      let tempArr = [];
+      let renderQueue = [];
+      pathArr.forEach((path) => {
+        const promise = this.findRoute(routes, {path: `/${path}`}).then((route) => {
+          return new Promise((resolve) => {
+            if (route.component) {
+              route.component().then((module) => {
+                contArr.push(module.default().context);
+                tempArr.push(module.default().template);
+                resolve();
+              })
+            }
+            if (route.redirect) {
+              this.pushRoute(routes, route);
+              resolve();
+            }
+          });
+        });
+        renderQueue.push(promise);
+      });
+      Promise.all(renderQueue).then(() => {
+        if (contArr.length && tempArr.length) {
+          this.renderAfterReload(contArr, tempArr);
+        }
+      });
+    },
+    renderAfterReload(contArr, tempArr) {
+      const routerView = document.querySelector('.j-router-view');
+      let container = document.createElement('div');
+      
+      tempArr.forEach((template) => {
+        const routerViewList = container.querySelectorAll('.j-router-view');
+        if (!routerViewList.length) {
+          container.innerHTML = template;
+        } else {
+          routerViewList[routerViewList.length - 1].innerHTML = template;
+        }
+      });
+      
+      routerView.innerHTML = container.innerHTML;
+      
+      contArr.forEach((context) => {
+        context();
+      })
+    },
+    pushRoute(routes, route) {
+      history.pushState({route: route.fullPath}, '', route.fullPath);
+      if (route.redirect) {
+        history.pushState({route: route.redirect.path}, '', route.redirect.path);
+        this.setRoute(routes, {path: route.redirect.path});
+      }
+      if (route.component) {
+        this.renderComponent(route);
+      }
+    },
+    renderComponent(route) {
+      const renderParams = this.genRenderParams(route);
+      const routerView = document.querySelectorAll('.j-router-view');
+      
+      route.component().then((module) => {
+        routerView[renderParams.index].innerHTML = module.default().template;
+        module.default().context();
+      });
+    },
+    genRenderParams(route) {
+      const pathArr = location.pathname.split('/').splice(1);
+      return {
+        pathLength: pathArr.length,
+        index: pathArr.indexOf(route.path.substring(1))
+      }
     }
-  });
-}
-
-function renderAfterReload(contArr, tempArr) {
-  const routerView = document.querySelector('.j-router-view');
-  let container = document.createElement('div');
-  
-  tempArr.forEach((template) => {
-    const routerViewList = container.querySelectorAll('.j-router-view');
-    if (!routerViewList.length) {
-      container.innerHTML = template;
-    } else {
-      routerViewList[routerViewList.length - 1].innerHTML = template;
-    }
-  });
-  
-  routerView.innerHTML = container.innerHTML;
-  
-  contArr.forEach((context) => {
-    context();
-  })
-}
-
-function pushRoute(routes, route) {
-  history.pushState({route: route.fullPath}, '', route.fullPath);
-  if (route.redirect) {
-    history.pushState({route: route.redirect.path}, '', route.redirect.path);
-    setRoute(routes, {path: route.redirect.path});
-  }
-  if (route.component) {
-    renderComponent(route);
-  }
-}
-
-function renderComponent(route) {
-  const renderParams = genRenderParams(route);
-  const routerView = document.querySelectorAll('.j-router-view');
-
-  route.component().then((module) => {
-    routerView[renderParams.index].innerHTML = module.default().template;
-    module.default().context();
-  });
-}
-
-function genRenderParams(route) {
-  const pathArr = location.pathname.split('/').splice(1);
-  return {
-    pathLength: pathArr.length,
-    index: pathArr.indexOf(route.path.substring(1))
   }
 }
