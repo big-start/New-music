3749f9fabaf8bc70dd962ec1ca1f1331f4dfe22e
hunter-s70 <serg.buhtoyarov@gmail.com>
2019-02-13 01:43:46 +0200

    add paginator on the top tracks page

--- src/pages/tracksPage.js
+++ src/pages/tracksPage.js
@@ -10,11 +10,28 @@ export default () => {
       artists: window.location.pathname.indexOf('artists') !== -1,
       tracks: window.location.pathname.indexOf('tracks') !== -1
     }),
-    context: ({$api}) => {
-      $api.getTopTracks().then((data) => {
-        const tracksTmp = topTracks({title: 'Top tracks list', data});
-        app.render({template: tracksTmp});
-      });
+    context: ({$, $api}) => {
+      function getTracks(page) {
+        $api.getTopTracks({page}).then((data) => {
+          const totalPages = data.tracks['@attr'].totalPages;
+          app.render({template: topTracks({
+            data,
+            title: 'Top tracks list',
+            page: {
+              isDisabledNext: page+1 > totalPages,
+              past: page-1,
+              current: page,
+              next: page+1,
+              total: totalPages
+            }
+          })}).then(() => {
+            $('.j-paginator-btn').on('click', function() {
+              getTracks($(this).data('type') === 'prev' ? page - 1 : page + 1);
+            });
+          });
+        });
+      }
+      getTracks(1);
     }
   });
 };
